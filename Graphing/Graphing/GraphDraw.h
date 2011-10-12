56a5ccc2e0ede320c743d2fcf28fcaa877626a8d
Nicole Erkis <nicoleerkis@gmail.com>
2011-10-11 21:23:08 -0400

    added graphdraw class

---
+++ Graphing/Graphing/GraphDraw.h
@@ -0,0 +1,13 @@
+//
+//  GraphDraw.h
+//  Graphing
+//
+//  Created by Nicole Erkis on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface GraphDraw : GraphDraw
+
+@end
---
+++ Graphing/Graphing/GraphDraw.m
@@ -0,0 +1,17 @@
+//
+//  GraphDraw.m
+//  Graphing
+//
+//  Created by Nicole Erkis on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GraphDraw.h"
+
+@implementation GraphDraw
+
+@synthesize delegate;
+
+//graphing drawing goes here!!
+
+@end
